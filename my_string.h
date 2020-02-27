50e104d5d9caaebd897809e83e3fab38db59c54f
Kushypoo <kushtrim_m2000@icloud.com>
2020-02-26 20:23:10 -0800

    Create my_string.h

---
+++ my_string.h
@@ -0,0 +1,7 @@
+#ifndef MY_STRING_H_
+#define MY_STRING_H_
+char *myStringCopy(char * destination, const char * source);
+size_t myStringLength(const char * str);
+int myStringCompare(const char *str1, const char *str2);
+char * myStringCat(char * destination, const char * source);
+#endif
