77b0c4aa56e9ed2c37a098f3ba673b82a5c7b155
Kushypoo <kushtrim_m2000@icloud.com>
2020-02-26 21:15:47 -0800

    Create proj4.cpp

---
+++ proj4.cpp
@@ -0,0 +1,22 @@
+#define MAX 256	
+#define DFT_YEAR 0
+#define DFT_PRICE 0.0
+#define DFT_AVAILABLE 0
+
+#include <iostream>
+#include <fstream>
+
+#include "my_string.h"
+#include "Sensor.h"
+#include "Car.h"
+#include "Agency.h"
+#include "menu.h"
+
+
+using namespace std;
+
+
+int main(){
+	userMenuPrompt();
+	return 0;
+}
