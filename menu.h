aaa81a47016ac4b01bac60d21b391da0cb739be9
Kushypoo <kushtrim_m2000@icloud.com>
2020-02-26 21:21:21 -0800

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H_
+#define MENU_H_
+
+void userMenuPrompt();	
+void readIn(class Agency *arr);
+void printAll(class Agency *arr);
+void printFiltered(class Agency *arr);
+void reserveOne(class Agency *arr);
+
+#endif
